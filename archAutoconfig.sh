c55317e044148f038f59dc9e15e888e298493ba0
tbl4h <45106120+tbl4h@users.noreply.github.com>
2025-07-03 14:51:51 +0200

    Update archAutoconfig.sh

--- archAutoconfig.sh
+++ archAutoconfig.sh
@@ -29,7 +29,7 @@ sudo pacman -S python python-pip
 sudo pacman -S python-pynvim
 sudo pacman -S fd
 sudo pacman -S ripgrep
-sudo pacman -S rust cargo
+sudo pacman -S rust cargo rustfmt
 cargo install selene
 cargo install stylua
 
